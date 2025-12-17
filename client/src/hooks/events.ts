e6e39dbddecf7209d43e42ac1c1741a42d7ee10e
phillip.nguyen <phillip.nguyen272@gmail.com>
2025-12-17 19:09:06 +0800

    modified hook to ensure that event return type is specified

--- client/src/hooks/events.ts
+++ client/src/hooks/events.ts
@@ -2,11 +2,19 @@ import { useQuery, UseQueryOptions } from "@tanstack/react-query";
 
 import api from "../lib/api";
 
+export type Event = {
+  id: number;
+  event_name: string;
+  event_description: string;
+  event_date: string;
+  event_location: string;
+};
+
 // hook to fetch events list
 export const useEvents = (
-  args?: Omit<UseQueryOptions, "queryKey" | "queryFn">,
+  args?: Omit<UseQueryOptions<Event[]>, "queryKey" | "queryFn">,
 ) => {
-  return useQuery({
+  return useQuery<Event[]>({
     ...args,
     queryKey: ["events"],
     queryFn: () => api.get("/event/").then((res) => res.data),
