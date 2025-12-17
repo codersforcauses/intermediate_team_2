6ba508fbd598cb2fb87ccc81ea92f244553881d9
phillip.nguyen <phillip.nguyen272@gmail.com>
2025-12-17 19:14:18 +0800

    added home functionality to fetch/display events

--- client/src/pages/home.tsx
+++ client/src/pages/home.tsx
@@ -1,11 +1,39 @@
+import { useEvents } from "@/hooks/events";
+
+import { Button } from "../components/ui/button";
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from "../components/ui/card";
 import Layout from "./layout";
 
 export default function Home() {
+  const { data: events, isLoading } = useEvents();
+
   return (
     <Layout>
-      <div className="p-6">
-        <h1 className="text-2xl font-bold">Home</h1>
-        <p>This is the Home page.</p>
+      <div className="mx-auto max-w-7xl space-y-3 px-4 py-6">
+        {!isLoading &&
+          events?.map((event) => (
+            <Card key={event.id}>
+              <CardHeader>
+                <CardTitle>{event.event_name}</CardTitle>
+                <CardDescription>{event.event_location}</CardDescription>
+              </CardHeader>
+
+              <CardContent>
+                <p>{event.event_description}</p>
+              </CardContent>
+
+              <CardFooter className="flex justify-end gap-2">
+                <Button>Signup</Button>
+              </CardFooter>
+            </Card>
+          ))}
       </div>
     </Layout>
   );
