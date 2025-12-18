7d94de938698bad05a7f4f97d74b5fc7cd7229a1
phillip.nguyen <phillip.nguyen272@gmail.com>
2025-12-18 11:30:15 +0800

    moved home to use docklayout, search bar placed above

--- client/src/pages/home.tsx
+++ client/src/pages/home.tsx
@@ -9,32 +9,59 @@ import {
   CardHeader,
   CardTitle,
 } from "../components/ui/card";
-import Layout from "./layout";
+import DockLayout from "./layouts/docklayout";
 
 export default function Home() {
   const { data: events, isLoading } = useEvents();
 
   return (
-    <Layout>
-      <div className="mx-auto max-w-7xl space-y-3 px-4 py-6">
-        {!isLoading &&
-          events?.map((event) => (
-            <Card key={event.id}>
-              <CardHeader>
-                <CardTitle>{event.event_name}</CardTitle>
-                <CardDescription>{event.event_location}</CardDescription>
-              </CardHeader>
+    <>
+      <DockLayout
+        bottomOverlay={
+          <div className="grid grid-cols-10 bg-green-100">
+            <div className="col-span-8 grid items-center gap-2 rounded-full bg-purple-100 bg-white px-4 py-3 shadow-lg">
+              <div className="rounded-full bg-red-100 px-4 py-2">
+                <input
+                  type="text"
+                  placeholder="Search events"
+                  className="w-full bg-transparent text-sm outline-none"
+                />
+              </div>
+            </div>
+            <div className="col-span-1 flex justify-end gap-2">
+              <button className="w-full rounded-full bg-blue-100 p-2 hover:bg-gray-100">
+                🔍
+              </button>
+            </div>
+            <div className="col-span-1 flex justify-end gap-2">
+              <button className="w-full rounded-full bg-blue-100 p-2 hover:bg-gray-100">
+                🔍
+              </button>
+            </div>
+          </div>
+        }
+      >
+        <div className="mx-auto max-w-7xl space-y-3 px-4 py-6">
+          {!isLoading &&
+            events?.map((event) => (
+              <Card key={event.id}>
+                <CardHeader>
+                  <CardTitle>{event.event_name}</CardTitle>
+                  <CardDescription>{event.event_location}</CardDescription>
+                </CardHeader>
 
-              <CardContent>
-                <p>{event.event_description}</p>
-              </CardContent>
+                <CardContent>
+                  <p>{event.event_description}</p>
+                </CardContent>
 
-              <CardFooter className="flex justify-end gap-2">
-                <Button>Signup</Button>
-              </CardFooter>
-            </Card>
-          ))}
-      </div>
-    </Layout>
+                <CardFooter className="flex-end flex-row justify-end gap-2">
+                  <Button>Signup</Button>
+                  <Button>Signup</Button>
+                </CardFooter>
+              </Card>
+            ))}
+        </div>
+      </DockLayout>
+    </>
   );
 }
