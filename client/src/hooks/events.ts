d1ce959d4e8acf7d7dc1540ed63b9ed4ef5b1d9f
phillip.nguyen <phillip.nguyen272@gmail.com>
2025-12-18 21:29:39 +0800

    updated useEvents hook to return an array of Tags

--- client/src/hooks/events.ts
+++ client/src/hooks/events.ts
@@ -2,13 +2,18 @@ import { useQuery, UseQueryOptions } from "@tanstack/react-query";
 
 import api from "../lib/api";
 
-export type Event = {
+export interface Tag {
+  id: number;
+  name: string;
+}
+export interface Event {
   id: number;
   event_name: string;
   event_description: string;
   event_date: string;
   event_location: string;
-};
+  tags: Tag[];
+}
 
 // hook to fetch events list
 export const useEvents = (
