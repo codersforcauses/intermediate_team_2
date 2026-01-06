647b108b563ea880af91836c982209e53c4c338d
phillip.nguyen <phillip.nguyen272@gmail.com>
2026-01-06 08:55:05 +0800

    merge will -> phillip. implements a profile view

--- server/user/views.py
+++ server/user/views.py
@@ -9,21 +9,24 @@ from rest_framework.permissions import AllowAny
 from .models import Profile, User
 from .serializers import ProfileSerializer, UserSerializer, RegisterSerializer
 
+from rest_framework.permissions import AllowAny # 暂时加上
+
 # Create your views here.
 
 class UserList(APIView):
-    permission_classes = (permissions.IsAuthenticated,)
+    permission_classes = (AllowAny,)#permissions.IsAuthenticated
     def get(self, request):
         users = User.objects.all()
         serializer = UserSerializer(users, many=True)
         return Response(serializer.data)
     
 class UserProfileList(generics.ListAPIView):
+    permission_classes = (AllowAny,)
     queryset = Profile.objects.all()
     serializer_class = ProfileSerializer
 
 class UserProfileDetail(generics.RetrieveUpdateAPIView):
-    permission_classes = (permissions.IsAuthenticated, IsUserOrReadOnly)
+    permission_classes = (AllowAny, )
     queryset = Profile.objects.all()
     serializer_class = ProfileSerializer
 
